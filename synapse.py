93081c440009a5111680b880685e6ebfd31721aa
techknowdgeteam <techknowdgeteam@gmail.com>
2026-02-15 17:11:04 +0100

    Add files via upload

--- synapse.py
+++ synapse.py
@@ -1,1 +1,50 @@
 import ohlc
+import techniques
+import calculateprices
+import placeorders
+import demo_placeorders
+import time
+
+def fetch_ohlc():
+    try:
+        ohlc.main()
+        print("ohlc completed.")
+    except Exception as e:
+        print(f"Error in ohlc: {e}")
+
+def technical_analysis():
+    try:
+        techniques.main()
+        print("technical analysis completed.")
+    except Exception as e:
+        print(f"Error in techniques: {e}")
+
+def calculate_prices():
+    try:
+        calculateprices.calculate_orders()
+    except Exception as e:
+        print(f"Error in calculateprices: {e}")
+
+def place_orders():
+    try:
+        placeorders.place_orders()
+        print("Placing real account orders completed.")
+    except Exception as e:
+        print(f"Error in placeorders: {e}")
+
+def place_demo_orders():
+    try:
+        demo_placeorders.place_orders()
+        print("Placing demo account orders completed.")
+    except Exception as e:
+        print(f"Error in placedemoorders: {e}")
+
+def main():
+    fetch_ohlc()
+    technical_analysis()
+    calculate_prices()
+    place_demo_orders()
+    time.sleep(60)
+main()
+
+
